8145bad2f3acc9843affe2236bc17692e30338bd
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-26 09:54:16 +0200

    Added a New Project

---
+++ 04. Fold and Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int operations = inputArray.Length / 2;
+
+int sideOperations = operations / 2;
+
+int[] outputArray = new int[operations];
+
+int count = 0;
+
+int leftCount = sideOperations;
+
+int rightCount = operations;
+
+for (int i = sideOperations - 1; i >= 0; i--)
+{
+    outputArray[count] = inputArray[i] + inputArray[leftCount];
+    leftCount++;
+    count++;
+
+}
+count = outputArray.Length - sideOperations;
+for (int i = inputArray.Length - 1; i >= inputArray.Length - sideOperations; i--)
+{
+
+    outputArray[count] = inputArray[i] + inputArray[rightCount];
+    rightCount++;
+    count++;
+}
+
+Console.WriteLine(String.Join(" ", outputArray));
