815586efb8f9761119a50c01080fdc605f8494aa
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-26 07:33:27 +0200

    Add project files.

---
+++ 01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] encryptedValues = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+    int length = input.Length;
+    int sum = 0;
+
+    foreach (char c in input)
+    {
+        char lowerChar = char.ToLower(c);
+        if ("aeiou".Contains(lowerChar)) 
+        {
+            sum += (int)c * length;
+        }
+        else 
+        {
+            sum += (int)c / length;
+        }
+    }
+
+    encryptedValues[i] = sum;
+}
+
+Array.Sort(encryptedValues);
+
+foreach (int value in encryptedValues)
+{
+    Console.WriteLine(value);
+}
---
+++ 02. Pascal Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿int input = int.Parse(Console.ReadLine());
+int[] array1 = { 1 };
+Console.WriteLine(1);
+
+for (int i = 1; i <= input - 1; i++)
+{
+
+    int[] array2 = new int [array1.Length + 1];
+    array2[0] = 1;
+    array2[array2.Length -1] = 1;
+    if (array2.Length > 2)
+    {
+        for (int j = 1; j < array2.Length - 1; j++)
+        {
+            array2[j] = array1[j - 1] + array1[j];
+        }
+    }
+    array1 = array2.ToArray();
+    Console.WriteLine(String.Join(" ", array1));
+}
